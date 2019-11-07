56041a5c79410c67100a21127686ff57880c2908
okFox <smayfield@gmail.com>
2019-11-07 11:22:08 -0800

    assets

--- data/monsters.js
+++ data/monsters.js
@@ -5,48 +5,56 @@ module.exports = [
         hp: 225,
         isLegendary: true,
         alignment: 'Lawful Evil',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Adult Silver Dragon',
         hp: 243,
         isLegendary: true,
         alignment: 'Lawful Good',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Ancient Black Dragon',
         hp: 367,
         isLegendary: true,
         alignment: 'Chaotic Evil',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Death Dog',
         hp: 39,
         isLegendary: false,
         alignment: 'Neutral Evil',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Dryad',
         hp: 22,
         isLegendary: false,
         alignment: 'Neutral',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Giant Centipede',
         hp: 4,
         isLegendary: false,
         alignment: 'Neutral',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Hill Giant',
         hp: 105,
         isLegendary: false,
         alignment: 'Chaotic Evil',
+        url: 'assets/sarah-pic.png',
     },
     {
         name: 'Tyrannosaurus Rex',
         hp: 136,
         isLegendary: false,
         alignment: 'Unaligned',
+        url: 'assets/sarah-pic.png',
     }
 
 ];
