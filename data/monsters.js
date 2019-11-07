ee7c0fd1da359148b18bdda29dd5975dc825326e
okFox <smayfield@gmail.com>
2019-11-07 10:09:19 -0800

    monster data

---
+++ data/monsters.js
@@ -0,0 +1,52 @@
+
+module.exports = [
+    {
+        name: 'Adult Blue Dragon',
+        hp: 225,
+        isLegendary: true,
+        alignment: 'Lawful Evil',
+    },
+    {
+        name: 'Adult Silver Dragon',
+        hp: 243,
+        isLegendary: true,
+        alignment: 'Lawful Good',
+    },
+    {
+        name: 'Ancient Black Dragon',
+        hp: 367,
+        isLegendary: true,
+        alignment: 'Chaotic Evil',
+    },
+    {
+        name: 'Death Dog',
+        hp: 39,
+        isLegendary: false,
+        alignment: 'Neutral Evil',
+    },
+    {
+        name: 'Dryad',
+        hp: 22,
+        isLegendary: false,
+        alignment: 'Neutral',
+    },
+    {
+        name: 'Giant Centipede',
+        hp: 4,
+        isLegendary: false,
+        alignment: 'Neutral',
+    },
+    {
+        name: 'Hill Giant',
+        hp: 105,
+        isLegendary: false,
+        alignment: 'Chaotic Evil',
+    },
+    {
+        name: 'Tyrannosaurus Rex',
+        hp: 136,
+        isLegendary: false,
+        alignment: 'Unaligned',
+    }
+
+];
