f12a981dcceda1f651f0ec4e4a49956c4902af0a
okFox <smayfield@gmail.com>
2019-11-08 09:48:49 -0800

    pathing

--- data/monsters.js
+++ data/monsters.js
@@ -5,21 +5,21 @@ module.exports = [
         hp: 225,
         isLegendary: true,
         alignment: 'Lawful Evil',
-        url: '../public/assets/adult-blue-dragon.jpg',
+        url: './public/assets/adult-blue-dragon.jpg',
     },
     {
         name: 'Adult Silver Dragon',
         hp: 243,
         isLegendary: true,
         alignment: 'Lawful Good',
-        url: '../public/assets/adult-silver-dragon.jpeg',
+        url: './public/assets/adult-silver-dragon.jpeg',
     },
     {
         name: 'Ancient Black Dragon',
         hp: 367,
         isLegendary: true,
         alignment: 'Chaotic Evil',
-        url: '../public/assets/ancient-black-dragon.jpg',
+        url: './public/assets/ancient-black-dragon.jpg',
     },
     {
         name: 'Death Dog',
@@ -40,21 +40,21 @@ module.exports = [
         hp: 4,
         isLegendary: false,
         alignment: 'Neutral',
-        url: '../public/assets/giant-centipede.jpg',
+        url: './public/assets/giant-centipede.jpg',
     },
     {
         name: 'Hill Giant',
         hp: 105,
         isLegendary: false,
         alignment: 'Chaotic Evil',
-        url: '../public/assets/hill-giant.jpeg',
+        url: './public/assets/hill-giant.jpeg',
     },
     {
         name: 'Tyrannosaurus Rex',
         hp: 136,
         isLegendary: false,
         alignment: 'Unaligned',
-        url: '../public/assets/t-rex.png',
+        url: './public/assets/t-rex.png',
     }
 
 ];
