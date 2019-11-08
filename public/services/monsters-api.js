d8d49da859e187b23974109a62da88e121368958
okFox <smayfield@gmail.com>
2019-11-08 11:12:09 -0800

    monsterform

--- public/services/monsters-api.js
+++ public/services/monsters-api.js
@@ -7,3 +7,24 @@ export async function getMonsters() {
     const data = await response.json();
     return data;
 }
+
+export function getOneMonster(id) {  
+    const url = `${URL}/monsters/${id}`;
+    return fetch(url)
+        .then(response => response.json());
+}
+
+export async function addMonster(monster) {
+    const url = `${URL}/monsters`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(monster)
+    });
+
+    const data = await response.json();
+    return data;
+}
