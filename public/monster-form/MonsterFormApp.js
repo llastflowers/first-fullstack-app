269cea6af3776813c6b6f8f0a129fe14c0c3dcde
Brittany Houtz <brittany.houtz@gmail.com>
2019-11-08 13:06:27 -0800

    form page header rendering

---
+++ public/monster-form/MonsterFormApp.js
@@ -0,0 +1,31 @@
+import Component from '../Component.js';
+import Header from '../common/Header.js';
+import MonsterForm from './MonsterForm.js';
+import { getOneMonster } from '../services/monsters-api.js';
+
+class MonsterFormApp extends Component {
+
+    async onRender(dom) {
+        const header = new Header({ title: 'D&D MONSTERS' });
+        dom.prepend(header.renderDOM());
+
+        const main = dom.querySelector('main');
+
+        const alignments = await getOneMonster();
+        const monsterForm = new MonsterForm({ alignments });
+        main.appendChild(monsterForm.renderDOM());
+    }
+
+    renderHTML() {
+        return /*html*/`
+            <div>
+                <!-- header goes here -->
+                <main>
+                    
+                </main>
+            </div>
+        `;
+    }
+}
+
+export default MonsterFormApp;
