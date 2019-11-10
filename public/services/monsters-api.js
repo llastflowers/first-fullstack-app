38be48ef39c4e97917d1a7e933369b258ec331f4
okFox <smayfield@gmail.com>
2019-11-09 20:44:38 -0800

    single monster endpoint

--- public/services/monsters-api.js
+++ public/services/monsters-api.js
@@ -17,9 +17,16 @@ export async function getAlignment() {
     return data;
 }
 
+export async function getOneMonster(id) {
+    const url = `${URL}/monsters/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
 
 //this one is not wired to anything
-// export function getOneMonster(id) {  
+// export async function getOneMonster(id) {  
 //     const url = `${URL}/monsters/${id}`;
 //     return fetch(url)
 //         .then(response => response.json());
