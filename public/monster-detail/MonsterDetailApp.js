fe44fe009bc5b006b3de5bb50fa1468cf7d275b6
Brittany Houtz <brittany.houtz@gmail.com>
2019-11-11 11:03:44 -0800

    style detail page

--- public/monster-detail/MonsterDetailApp.js
+++ public/monster-detail/MonsterDetailApp.js
@@ -36,23 +36,10 @@ class MonsterDetailApp extends Component {
     }
 
     renderHTML() {
-        const monster = this.props.monster;
-
         return /*html*/`
             <div>
                 <!-- header goes here -->
                 <main>
-                <section class="monster-item">
-                <h2>${monster.name}</h2>
-                <div class="image-container">
-                    <img src="${monster.url}" alt="${monster.name} image">
-                </div>
-                <div class="info-container">
-                    <p class="monster-hp">HP: ${monster.hp}</p>
-                    <p class="monster-alignment">Alignment: ${monster.alignment}</p>
-                    <p class="monster-is-legendary">Legendary: ${monster.isLegendary}</p>
-                </div>
-                </section>
                 </main>
             </div>
         `;
