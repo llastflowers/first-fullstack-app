e5e8b09f28e2da94a23c2d826c32b01861927603
okFox <smayfield@gmail.com>
2019-11-10 17:09:20 -0800

    typos

--- public/services/monsters-api.js
+++ public/services/monsters-api.js
@@ -2,7 +2,6 @@ const URL = '/api';
 
 export async function getMonsters() {
     const url = `${URL}/monsters`;
-
     const response = await fetch(url);
     const data = await response.json();
     return data;
@@ -19,7 +18,7 @@ export async function addMonster(monster) {
     const url = `${URL}/monsters`;
 //the following line is giving error?
     const response = await fetch(url, {
-        method: 'post',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
